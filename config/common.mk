acb0ddc5043e9761080b80b99895cf3443882495
Jorge Pereira <jgcaap@gmail.com>
2015-05-11 07:19:42 +0200

    Add SuperSU 2.52
    Change-Id: Ib3e89bb8dbc5d43c4a6e2a9f89f16906caed78a5

--- config/common.mk
+++ config/common.mk
@@ -64,6 +64,12 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
 
+# SuperSU
+PRODUCT_COPY_FILES += \
+   vendor/cm/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+   vendor/cm/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
+
 # Backup Tool
 ifneq ($(WITH_GMS),true)
 PRODUCT_COPY_FILES += \
