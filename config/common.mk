da587e2a7b0e45a55a9ee3e511454b1853ab3423
jgcaap <jgcaap@gmail.com>
2015-05-25 17:08:52 -0400

    Su
    Change-Id: If895838e41ed4469cb682a7eccb31175949e40a7

--- config/common.mk
+++ config/common.mk
@@ -98,7 +98,7 @@ PRODUCT_COPY_FILES +=  \
 
 # SuperSU
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/supersu.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/cm/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
     vendor/cm/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 
 # Enable SIP+VoIP on all targets
