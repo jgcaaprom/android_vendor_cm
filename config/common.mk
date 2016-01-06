3960b886db436cb90821593973adc8375b8031a5
Jorge Pereira <jgcaap@gmail.com>
2016-01-06 00:30:45 +0000

    Add Snapdragon Camera
    Change-Id: I05007b6c097a295fc17584713b5b4d89b5891dad

--- config/common.mk
+++ config/common.mk
@@ -147,7 +147,8 @@ PRODUCT_PACKAGES += \
     CyanogenSetupWizard \
     CMSettingsProvider \
     ExactCalculator \
-    CameraNext
+    CameraNext \
+    SnapdragonCamera
 
 # Exchange support
 PRODUCT_PACKAGES += \
