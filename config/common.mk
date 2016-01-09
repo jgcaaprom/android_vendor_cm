b63752b1ff7a8a2919c619e9be50457ecbed4405
Jorge Pereira <jgcaap@gmail.com>
2016-01-09 20:13:42 +0000

    Set persist.sys.root_access=3
    Change-Id: I8d97f2e6598894f30cb27de87857c211037e71c1

--- config/common.mk
+++ config/common.mk
@@ -199,7 +199,7 @@ PRODUCT_PACKAGES += \
     su
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.root_access=0
+    persist.sys.root_access=3
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
