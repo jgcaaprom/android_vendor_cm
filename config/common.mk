c5e603db31466c6d8a6d3811dc4afa156d538c15
Jorge Pereira <jgcaap@gmail.com>
2016-02-01 12:24:36 -0600

    Disable CyanogenMod Setup This is anoying option as doesn't let users to automatically install apps after setup. Disable for now.

--- config/common.mk
+++ config/common.mk
@@ -43,7 +43,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false
 
@@ -132,7 +131,6 @@ PRODUCT_PACKAGES += \
     Eleven \
     LockClock \
     CMAccount \
-    CyanogenSetupWizard \
     CMSettingsProvider \
     ExactCalculator \
     Snap
