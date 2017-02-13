04ef8a39860085c4762d96f351a4d6d306a0d777
Jorge Ayres Pereira <jgcaap@gmail.com>
2017-02-13 14:20:02 +0100

    changes

--- config/common.mk
+++ config/common.mk
@@ -284,7 +284,7 @@ ifdef CM_BUILDTYPE
     endif
 else
     # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
-    CM_BUILDTYPE := UNOFFICIAL
+    CM_BUILDTYPE := jgcaap
     CM_EXTRAVERSION :=
 endif
 
